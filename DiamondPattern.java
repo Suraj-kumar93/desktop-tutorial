c19a761234d9bb7ee397254f7b48615b8ebad48a
Suraj-kumar93 <surajbharangar9@gmail.com>
2026-09-03 09:19:11 +0530

    Add files via upload

---
+++ DiamondPattern.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class DiamondPattern {
+     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int n = sc.nextInt();
+
+       
+        for(int i = 1 ;i<=n;i++){
+            for(int k=n-1;k>=i;k--){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+              for(int l = 2; l <= i; l++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+        
+        for (int i = n - 1; i >= 1; i--) {
+
+            // Spaces
+            for (int k = n - 1; k >= i; k--) {
+                System.out.print(" ");
+            }
+
+            // Stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            for (int l = 2; l <= i; l++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+
+}
